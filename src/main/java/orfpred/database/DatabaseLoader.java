04b5dbcb9a8704a5352e376931fdfc3572473a4a
grimcode <a.janse@student.han.nl>
2018-03-24 16:12:54 +0100

    test main toegevoegd bij dbloader

--- src/main/java/orfpred/database/DatabaseLoader.java
+++ src/main/java/orfpred/database/DatabaseLoader.java
@@ -19,19 +19,28 @@ import java.util.MissingResourceException;
 
 /**
  * Class om data uit de database in te laden.
- *
- * @author Projectgroep 9
+ * 
+ * @author Projectgroep 10
  */
 public class DatabaseLoader {
 
     private final DatabaseConnector connector;
 
+    
+    public static void main(String[] args) {
+        try{
+        DatabaseLoader loader = new DatabaseLoader();
+            System.out.println("dadas");
+        } catch (Exception e){
+            System.out.println(e.toString());
+        }
+    }
     /**
      * Constructor
-     *
+     * 
      * @throws SQLException
      * @throws ClassNotFoundException
-     * @throws MissingResourceException
+     * @throws MissingResourceException 
      */
     public DatabaseLoader() throws SQLException, ClassNotFoundException, MissingResourceException {
         this.connector = new DatabaseConnector();
@@ -39,62 +48,72 @@ public class DatabaseLoader {
 
     }
 
-    public final ArrayList<ArrayList<String>> getStoredFileNames() throws SQLException {
+
+    public ArrayList<ArrayList<String>> getStoredFileNames()throws SQLException{
+
         ResultSet resultSet = connector.sentFeedbackQuery("SELECT * FROM BESTAND");
-        return parseResultSetToArray(new String[]{"BESTAND_ID", "NAAM"}, resultSet);
+        return parseResultSetToArray(new String[]{"BESTAND_ID","NAAM"},resultSet);
     }
 
-    public ArrayList<ArrayList<String>> getHeadersFromFile(String bestandID) throws SQLException, CompoundNotFoundException {
-        ResultSet resultSet = connector.sentFeedbackQuery("SELECT SEQ_ID, HEADER, "
-                + "SEQUENTIE FROM SEQUENTIE WHERE BESTAND_ID = " + bestandID);
-        return parseResultSetToArray(new String[]{"SEQ_ID", "HEADER", "SEQUENTIE"}, resultSet);
+    public ArrayList<ArrayList<String>> getHeadersFromFile(String bestandID)throws SQLException, CompoundNotFoundException{
+        ResultSet resultSet = connector.sentFeedbackQuery("SELECT SEQ_ID, HEADER, " +
+                "SEQUENTIE FROM SEQUENTIE WHERE BESTAND_ID = "+bestandID);
+        return parseResultSetToArray(new String[]{"SEQ_ID","HEADER","SEQUENTIE"},resultSet);
     }
 
-    public HashMap<String, ORF> getORFFromDB(String seqID, String seq) throws SQLException, ParseException {
+    public HashMap<String, ORF> getORFFromDB(String seqID, String seq) throws SQLException, ParseException{
         HashMap<String, ORF> orfList = new HashMap<>();
-        ResultSet resultSet = connector.sentFeedbackQuery("SELECT ORF_ID, FRAME, "
-                + "START_POS, END_POS FROM ORF WHERE SEQ_ID = " + seqID);
-        while (resultSet.next()) {
+        ResultSet resultSet = connector.sentFeedbackQuery("SELECT ORF_ID, FRAME, " +
+                "START_POS, END_POS FROM ORF WHERE SEQ_ID = "+seqID);
+        while (resultSet.next()){
             int start = Integer.parseInt(resultSet.getString("START_POS")),
                     end = Integer.parseInt(resultSet.getString("END_POS"));
             Frame frame = getFrame(resultSet);
             String orfID = resultSet.getString("ORF_ID");
-            orfList.put(orfID, new ORF(frame, seq.substring(start, end + 1), start, end));
+            orfList.put(orfID, new ORF(frame, seq.substring(start,end+1), start, end));
         }
         return orfList;
     }
 
-    public Frame getFrame(ResultSet resultSet) throws SQLException, ParseException {
+    public Frame getFrame(ResultSet resultSet) throws SQLException, ParseException{
         String frame = resultSet.getString("FRAME");
-        switch (frame) {
+        switch (frame){
             case "+1":
                 return Frame.ONE;
+
             case "+2":
                 return Frame.TWO;
+
             case "+3":
                 return Frame.THREE;
+
             case "-1":
                 return Frame.REVERSED_ONE;
+
             case "-2":
                 return Frame.REVERSED_TWO;
+
             case "-3":
                 return Frame.REVERSED_THREE;
+
+            default:
+                break;
         }
         throw new ParserException("Error: Unkown frame found!");
     }
 
-    public ArrayList<ArrayList<String>> getBlastResult(String orfID) throws SQLException {
-        ResultSet resultSet = connector.sentFeedbackQuery("SELECT * FROM BLAST_RESULTAAT WHERE ORF_ID = " + orfID);
-        return parseResultSetToArray(new String[]{"RESULT_ID", "DESCRIPTION", "BITSCORE",
-            "QUERY_COVERAGE", "EVALUE", "IDENTITY", "POSITIVES", "GAPS"}, resultSet);
+    public ArrayList<ArrayList<String>> getBlastResult(String orfID) throws SQLException{
+        ResultSet resultSet = connector.sentFeedbackQuery("SELECT * FROM BLAST_RESULTAAT WHERE ORF_ID = "+orfID);
+        return parseResultSetToArray(new String[]{"RESULT_ID","DESCRIPTION","BITSCORE",
+                "QUERY_COVERAGE","EVALUE","IDENTITY","POSITIVES","GAPS"},resultSet);
     }
 
-    public ArrayList<ArrayList<String>> parseResultSetToArray(String[] headers, ResultSet resultSet) throws SQLException {
+    public ArrayList<ArrayList<String>> parseResultSetToArray(String[] headers, ResultSet resultSet) throws SQLException{
         ArrayList<ArrayList<String>> createdArray = new ArrayList<>();
         int counter = 0;
-        while (resultSet.next()) {
+        while (resultSet.next()){
             createdArray.add(new ArrayList<>());
-            for (String header : headers) {
+            for(String header : headers){
                 createdArray.get(counter).add(resultSet.getString(header));
             }
             counter++;
