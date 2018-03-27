cda8548494c79abe7ed282988fae8f71be220680
grimcode <a.janse@student.han.nl>
2018-03-27 19:55:42 +0200

    dbdeleter class functioneel gemaakt'

--- src/main/java/orfpred/database/DatabaseDeleter.java
+++ src/main/java/orfpred/database/DatabaseDeleter.java
@@ -13,26 +13,45 @@ public class DatabaseDeleter {
     public DatabaseDeleter(String bestandID) throws SQLException, ClassNotFoundException{
         this.connector = new DatabaseConnector();
         this.bestandID = bestandID;
-        this.sequentieIDs = getIDsToRemove(bestandID,"SEQUENTIE","BESTAND_ID");
-        for(String seqid : this.sequentieIDs){
-            this.orfIDs.addAll(getIDsToRemove(seqid,"ORF","SEQ_ID"));
-        }
-        for(String orfid : this.orfIDs){
-            this.blastIDs.addAll(getIDsToRemove(orfid,"BLAST_RESULTAAT","ORF_ID"));
-        }
+        this.createIDsArrays();
+        this.deleteData();
 
     }
 
-    public ArrayList<String> getIDsToRemove(String lookUpID, String table, String lookUpColumn) throws SQLException{
+    private void createIDsArrays() throws SQLException{
+        this.sequentieIDs = getIDsToRemove(bestandID,"SEQUENTIE","BESTAND_ID");
+        for(String seqid : this.sequentieIDs){
+            System.out.println(5);
+            this.orfIDs.addAll(getIDsToRemove(seqid,"ORF","SEQ_ID"));
+        }
+        for(String orfid : this.orfIDs){
+            System.out.println(6);
+            this.blastIDs.addAll(getIDsToRemove(orfid,"BLAST_RESULTAAT","ORF_ID"));
+        }
+    }
+
+    private void deleteData() throws SQLException{
+        deleteRowsByArray(blastIDs,"BLAST_RESULTAAT","RESULT_ID");
+        System.out.println(2);
+        deleteRowsByArray(orfIDs,"ORF","ORF_ID");
+        System.out.println(3);
+        deleteRowsByArray(sequentieIDs,"SEQUENTIE","SEQ_ID");
+        System.out.println(4);
+        connector.sentDeleteQuery("BESTAND","BESTAND_ID ="+bestandID);
+    }
+
+    private ArrayList<String> getIDsToRemove(String lookUpID, String table, String lookUpColumn) throws SQLException{
         ArrayList<String> foundIDs = new ArrayList<>();
         ResultSet resultSet = connector.sentFeedbackQuery("SELECT * FROM "+table+" WHERE "+lookUpColumn+" = "+lookUpID);
         while (resultSet.next()){
+            System.out.println(8);
+            System.out.println(1);
             foundIDs.add(resultSet.getString(1));
         }
         return foundIDs;
     }
 
-    public void deleteRowsByArray(ArrayList<String> ids, String table, String column) throws SQLException{
+    private void deleteRowsByArray(ArrayList<String> ids, String table, String column) throws SQLException{
         for(String id : ids){
             connector.sentDeleteQuery(table,column+" = "+id);
         }
